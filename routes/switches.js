af80c16e29ca3066e902a6fc5c041e9271b4f0e7
James Baxter <j.w.baxter@gmail.com>
2015-08-12 17:44:54 +0000

    Added some structure by factoring out dao and rcswitch

--- routes/switches.js
+++ routes/switches.js
@@ -1,36 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var rcswitch = require('rcswitch');
-var rcswitchDataPin = 0;
-var canTransmit = rcswitch.enableTransmit(rcswitchDataPin)
-console.log('Can transmit: ' + canTransmit)
-
-var MongoClient = require('mongodb').MongoClient
-var url = 'mongodb://localhost:27017/switch-server'
-
-function withDatabase(callback) {
-  MongoClient.connect(url, function(err, db) {
-    if (err)
-      return callback(err)
-    console.log('Connected to server')
-    callback(null, db)
-  })
-}
-
-function insertSwitch(aSwitch) {
-  withDatabase(function(err, db) {
-    if (err)
-      return callback(err)
-    var collection = db.collection('switches')
-    collection.insert(aSwitch)
-    db.close()
-  })
-}
+var switchDao = require('../dao/switchDao.js')
+var switchService = require('../services/switchService.js')
 
 router.param('switch', function(req, res, next, switchName) {
   console.log('Loading switch from database')
-  getSwitch(switchName, function(err, existingSwitch) {
+  switchDao.getOne(switchName, function(err, existingSwitch) {
     if (err) {
       return next(err)
     }
@@ -39,54 +15,24 @@ router.param('switch', function(req, res, next, switchName) {
   })
 })
 
-function getSwitch(name, callback) {
-  withDatabase(function(err, db) {
-    if (err)
-      return callback(err)
-    var collection = db.collection('switches')
-    collection.findOne({'name' : name}, {}, function(err, existingSwitch) {
-      console.log('Got switch ' + existingSwitch)
-      callback(null, existingSwitch)
-      db.close()
-    })
-  })
-}
-
-function listSwitches(callback) {
-  withDatabase(function(err, db) {
-    if (err)
-      return callback(err)
-    var collection = db.collection('switches')
-    collection.find({}).toArray(function(err, switches) {
-      if (err)
-        return callback(err)
-      console.log('Found the following switches')
-      console.dir(switches)
-      callback(null, switches)
-      db.close()
-    })
-  })
-}
-
-/* GET switches listing. */
-router.get('/', function(req, res, next) {
-  listSwitches(function(err, switches) {
-    if (err) {
-      res.sendStatus(500)
-      return console.error(err)
-    }
-    res.send(switches)
-  })
-});
-
 router.post('/', function(req, res, next) {
   var newSwitch = {
     'name' : req.body.name,
     'group' : req.body.group,
     'switch' : req.body.switch
   }
-  insertSwitch(newSwitch)
+  switchDao.create(newSwitch)
   res.sendStatus(204)
+});
+
+router.get('/', function(req, res, next) {
+  switchDao.getAll(function(err, switches) {
+    if (err) {
+      res.sendStatus(500)
+      return console.error(err)
+    }
+    res.send(switches)
+  })
 });
 
 router.patch('/:switch', function(req, res, next) {
@@ -96,9 +42,9 @@ router.patch('/:switch', function(req, res, next) {
   console.log('Switch [' + existingSwitch.name + '] on [' + switchOn + ']')
   console.dir(existingSwitch)
   if (switchOn) {
-    rcswitch.switchOn(existingSwitch.group, Number(existingSwitch.switch))
+    switchService.on(existingSwitch.group, Number(existingSwitch.switch))
   } else {
-    rcswitch.switchOff(existingSwitch.group, Number(existingSwitch.switch))
+    switchService.off(existingSwitch.group, Number(existingSwitch.switch))
   }
   res.sendStatus(204)
 });
