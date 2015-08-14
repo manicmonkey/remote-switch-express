b2c8b992e35cfbace43e419d847242506c6af1cc
James Baxter <j.w.baxter@gmail.com>
2015-08-14 19:56:42 +0000

    Combine switch handling and respond on content-type. Also create initial webpage with jquery and bootstrap

--- routes/switches.js
+++ routes/switches.js
@@ -17,23 +17,51 @@ router.param('switch', function(req, res, next, switchName) {
 
 router.post('/', function(req, res, next) {
   var newSwitch = {
-    'name' : req.body.name,
-    'group' : req.body.group,
+    name: req.body.name,
+    group : req.body.group,
     'switch' : req.body.switch
   }
   switchDao.create(newSwitch)
   res.sendStatus(204)
-});
+})
 
 router.get('/', function(req, res, next) {
   switchDao.getAll(function(err, switches) {
     if (err) {
-      res.sendStatus(500)
-      return console.error(err)
+      res.sendStatus(500) //todo send error page
+      return next(err)
     }
-    res.send(switches)
+
+    if (req.accepts('html')) {
+      res.render('switches', { title: 'Switch listing', switches: switches })
+      return
+    }
+
+    if (req.accepts('json')) {
+      res.send(switches)
+      return
+    }
+
+    res.sendStatus(406)
   })
-});
+})
+
+router.get('/:switch', function(req, res, next) {
+  console.log('Getting switch')
+  console.dir(req.headers)
+  if (req.accepts('html')) {
+    res.render('switch', { title: 'Switch management', name: req.switch.name })
+    return
+  }
+
+  if (req.accepts('json')) {
+    res.send(req.switch)
+    return
+  }
+
+  console.log("Content type not supported")
+  res.sendStatus(406)
+})
 
 router.patch('/:switch', function(req, res, next) {
   console.dir(req.body)
